d0bb0f77955a6369835b071dc1e8349a59693dfc
bodenlosus <johannes.till.schmidt@outlook.de>
2025-05-11 22:28:52 +0200

    a

--- zennote/app.py
+++ zennote/app.py
@@ -1,27 +1,34 @@
-
 import sys
 import gi
 import pathlib
+
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 gi.require_version("Panel", "1")
 from gi.repository import Gtk, GLib, Adw, Panel, Gio
 
-@Gtk.Template(resource_path='/ui/tab_bar.ui')
-class EditorTabBar(Gtk.Box):
-    __gtype_name__ = "EditorTabBar"
-    tab_view: Adw.TabView = Gtk.Template.Child("editor-tab-view")
+@Gtk.Template(resource_path="/ui/editor.ui")
+class Editor(Gtk.TextView):
+    __gtype_name__ = "Editor"
     def __init__(self):
-        page = Adw.TabPage(child=Gtk.Label(label="a"), title="a")
-        self.tab_view.add_page(Gtk.Label(label="b"), page)
         super().__init__()
+
+@Gtk.Template(resource_path="/ui/toolbar.ui")
+class EditorToolBar(Gtk.PopoverMenuBar):
+    __gtype_name__ = "EditorToolBar"
+
+    def __init__(self):
+        super().__init__()
+
 
 @Gtk.Template(resource_path="/ui/window.ui")
 class Window(Adw.ApplicationWindow):
     __gtype_name__ = "Window"
+
     def __init__(self, app):
         super().__init__(application=app)
         self.get_id
+
 
 class ZenNote(Adw.Application):
     def __init__(self):
@@ -30,8 +37,6 @@ class ZenNote(Adw.Application):
 
     def do_activate(self):
         GLib.set_application_name("zennote")
-    
-            
+
         window = Window(self)
         window.present()
-    
