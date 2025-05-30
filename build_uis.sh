cd44b100e305b5a281c4be2ae84b38b2d3ded4b7
bodenlosus <johannes.till.schmidt@outlook.de>
2025-05-30 11:03:03 +0200

    renamed to bracket

--- build_uis.sh
+++
@@ -1,6 +0,0 @@
-echo "building blueprints"
-blueprint-compiler batch-compile zennote/resources zennote/ui $(find zennote/ui/ -name '*.blp' -printf "%p ")
-echo "compiling resources"
-
-glib-compile-resources zennote/resources/ui.gresource.xml --sourcedir="zennote/resources/"
-# glib-compile-schemas data
--- zennote/utils.py
+++
@@ -1,5 +0,0 @@
-from typing import Any
-
-
-type Args = tuple[Any]
-type KwArgs = dict[str, Any]
